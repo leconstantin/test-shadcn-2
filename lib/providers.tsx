6eb06348dbf3b57634614e6d4c9d9616f675e984
Leo Constantin <lcon69184@gmail.com>
2025-09-05 14:18:04 +0200

    shadcn init

---
+++ lib/providers.tsx
@@ -0,0 +1,14 @@
+import { ThemeProvider } from '@/components/theme-provider';
+
+export default function Providers({ children }: { children: React.ReactNode }) {
+  return (
+    <ThemeProvider
+      attribute="class"
+      defaultTheme="system"
+      disableTransitionOnChange
+      enableSystem
+    >
+      {children}
+    </ThemeProvider>
+  );
+}
